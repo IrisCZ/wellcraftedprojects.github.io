a5cf4b6b31250d1dd2db7de6a8df45d28deb85b3
jjballano <jjballano@gmail.com>
2015-04-08 08:21:55 +0100

    Don't need this code

--- web/js/views/projectList.view.js
+++ web/js/views/projectList.view.js
@@ -17,15 +17,6 @@ ProjectListView = Backbone.View.extend({
   template: _.template($('#project_template').html()),
 
   render: function(){
-
-    console.log("PROJECTS")
-    console.log(this.projects.toJSON())
-    console.log(this.projects.at(1))
-//    var projects = new Projects([
-//          {image:"images/runu.png",author:'Author´s Name',description:'Project´s decription. Project´s decription. Project´s decription.',url:'https://github.com/runu/runu.github.io',positives:5,negatives:2},
-//          {image:"images/new-project.jpg",author:'Author´s Name',description:'Project´s decription. Project´s decription. Project´s decription.',url:'https://github.com/runu/runu.github.io',positives:5,negatives:2},
-//          {author:'Author´s Name',description:'Project´s decription. Project´s decription. Project´s decription.',url:'https://github.com/runu/runu.github.io',positives:5,negatives:2},
-//    ]);
     this.$el.html(this.template({projects:this.projects.toJSON()}))
   }
 
