fd6ba6360903bac3f0b6e2ea00bf426d62fa9426
jjballano <jjballano@gmail.com>
2015-04-12 15:55:17 +0100

    References updated

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -8,6 +8,7 @@ gulp.task('dependencies', function() {
     'node_modules/jquery/dist/jquery.min.js',
     'node_modules/underscore/underscore-min.js',
     'node_modules/backbone/backbone-min.js',
+    'node_modules/bootstrap/dist/js/bootstrap.min.js',
     'js/vendor/**/*.js'
   ];
 
