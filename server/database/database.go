6493435d32675d1201bcac7b6ab30109d877e095
jjballano <jjballano@gmail.com>
2015-08-13 05:52:48 +0100

    Changing server from Go to RoR

--- server/database/database.go
+++
@@ -1,9 +0,0 @@
-package database
-
-import "github.com/IrisCZ/wellcraftedprojects/model"
-
-type Database interface{
-  Save(obj model.Model, collectionName string) (string, error)
-  FindOne(collectionName string, params map[string]string, obj model.Model) error
-  FindAll(collectionName string) ([]map[string]interface{}, error)
-}
--- server/model/model.go
+++
@@ -1,6 +0,0 @@
-package model
-import "gopkg.in/mgo.v2/bson"
-
-type Model interface{
-    SetId(id bson.ObjectId)
-}
